52e96e484dd4d8b69496dd8da5b5d5463bb71162
Hitomi Shimura <hitomi_shimura@overtech.tokyo>
2025-07-09 22:07:42 +0900

    MUIを導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Button } from "@mui/material";
 
 export default function Page() {
   return (
     <div>
       <h1>Hello, Next.js!</h1>
       <Image src="/profile.png" alt="Profile" width={100} height={100} />
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
